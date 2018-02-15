720cb7952de93495d7df3de95387dd9b61d87eb5
ajzett <36409591+ajzett@users.noreply.github.com>
2018-02-14 20:25:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  //Prevent page moving left, right, up, or down with arrow keys.
   window.addEventListener('keydown', function(e) {
     if ((e.keycode >= 37) && (e.keycode <= 40)) {
       e.preventDefault();
